d226735850949ece29d111b3f59b55842c231d57
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-10-27 15:43:11 +0100

    Play random sounds test

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 window.customElements.define('div-touch', Touch);
 var main = document.getElementById('main');
 
-var square = new Touch('square','red');
-var triangle = new Touch('triangle','blue');
-var circle = new Touch('circle','green');
-var cross = new Touch('cross','yellow');
+var touch;
+
+var square = new Touch('square','purple');
+var triangle = new Touch('triangle','green');
+var circle = new Touch('circle','orange');
+var cross = new Touch('cross','blue');
 
 main.appendChild(square);
 main.appendChild(triangle);
@@ -25,9 +27,34 @@ cross.addEventListener('touchClicked', function (ev) {
     console.log(ev.detail);
 });
 
+//playRandomSounds();
+
 document.onclick = function(){
-	square.play().then(function(data){
-		console.log('playdone');
-		circle.play();
-	});
+	//touch.play().then(function(data){
+		//console.log('playdone');
+		playRandomSounds();
+	//});
 };
+
+function playRandomSounds() {
+	var random = Math.floor(Math.random()*4); //0,1,2,3
+
+	switch (random) {
+		case 0:
+			touch = square;
+			break;
+		case 1:
+			touch = triangle;
+			break;
+		case 2:
+			touch = circle;
+			break;
+		case 3:
+			touch = cross;
+			break;
+	}
+	touch.play().then(function(data){
+		console.log('playdone');
+		playRandomSounds();
+	});
+}
