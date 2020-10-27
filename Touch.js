1e6976c90b87bd6eee25d6077ee344b62a8ce19a
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-10-27 13:39:21 +0100

    Webcomponent

--- Touch.js
+++ Touch.js
@@ -1,13 +1,13 @@
 class Touch extends HTMLElement {
-	id = 0;
+	id;
 	sound;
 	bgColor;
-	opacity=0;
+	opacity = 0;
 	
 	
 	constructor (id) {
 		super();
-		this.id = id+1;
+		this.id = id;
 
 		this.initializeAttributes();
 		this.initListeners();
@@ -17,21 +17,25 @@ class Touch extends HTMLElement {
 
         switch(this.id) {
 			case 1 : 
-				sound = "./sounds/sound1.mp3";
-				bgColor = "green";
+				//sound = "./sounds/sound1.mp3";
+				this.bgColor = "green";
+				break;
 			case 2 : 
-				sound = "./sounds/sound2.mp3";
-				bgColor = "orange";
+				//sound = "./sounds/sound2.mp3";
+				this.bgColor = "orange";
+				break;
 			case 3 : 
-				sound = "./sounds/sound3.mp3";
-				bgColor = "blue";
+				//sound = "./sounds/sound3.mp3";
+				this.bgColor = "blue";
+				break;
 			case 4 : 
-				sound = "./sounds/sound4.mp3";
-				bgColor = "purple";
+				//sound = "./sounds/sound4.mp3";
+				this.bgColor = "purple";
+				break;
 		}
 
-        this.setAttribute("style", "opacity:0;");
-		this.setAttribute("style", "background-color:" + bgColor);
+        this.setAttribute("id", this.id);
+		this.setAttribute("style", "background-color:" + this.bgColor);
 
     }
 
@@ -41,12 +45,11 @@ class Touch extends HTMLElement {
             var event = new CustomEvent('touchClicked', {
 				detail: {
 					id: this.id,
-					sound: this.sound
+					//sound: this.sound
 				}
 			});
-			this.setAttribute("style", "opacity:0.7;");
+			this.style.opacity = 0.7;
             this.dispatchEvent(event);
         }
     }
 }
-window.customElements.define('touch', Touch);
---
+++ main.js
@@ -0,0 +1,17 @@
+window.customElements.define('div-touch', Touch);
+var main = document.getElementById('main');
+
+var touch1 = new Touch(1);
+var touch2 = new Touch(2);
+var touch3 = new Touch(3);
+var touch4 = new Touch(4);
+
+main.appendChild(touch1);
+main.appendChild(touch2);
+main.appendChild(touch3);
+main.appendChild(touch4);
+
+
+touch1.addEventListener('touchClicked', function (ev) {
+    console.log(touch1.detail);
+});
