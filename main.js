e2e0b7bc9b519d7de7914478b0c3805805abd845
Cindy Chiffoleau <64546974+erysfoly@users.noreply.github.com>
2020-10-28 13:39:22 +0100

    Melody

--- main.js
+++ main.js
@@ -1,100 +1,83 @@
-function start(){
-    $('#start').click(function () {
-        var val = $('#pseudo').val();
-        if (val != '') {
-            $('#game-container').attr('style', 'visibility:visible');
-            $('#game-img').attr('style', 'visibility:visible');
-            $('.start-container, .bg-img').hide();    
-        }else{
-            $('#error').attr('style', 'visibility:visible');
-        }
-        console.log(val);
-    });
-}
+$(document).ready(function(){
 
-start();
-
-window.customElements.define('div-touch', Touch);
-var main = document.getElementById('game-img');
-
-
-var square = new Touch('square','purple');
-var triangle = new Touch('triangle','green');
-var circle = new Touch('circle','orange');
-var cross = new Touch('cross','blue');
-
-main.appendChild(square);
-main.appendChild(triangle);
-main.appendChild(circle);
-main.appendChild(cross);
-
-
-square.addEventListener('touchClicked', function (ev) {
-    console.log(ev.detail);
-});
-triangle.addEventListener('touchClicked', function (ev) {
-    console.log(ev.detail);
-});
-circle.addEventListener('touchClicked', function (ev) {
-    console.log(ev.detail);
-});
-cross.addEventListener('touchClicked', function (ev) {
-    console.log(ev.detail);
-});
-
-//playRandomSounds();
-
-document.onclick = function(){
-	//touch.play().then(function(data){
-		//console.log('playdone');
-		//playRandomSounds();
-	//});
-	playMelody(melodyComputer).then(function(d){
-		console.log(d);
-	});
-};
-
-
-
-
-function playNote(note) {
-
-}
-
-
-
-
-function playMelody(melody){
-	// je promets de joueur TOUTE LA MELODIE
-
-	
-		// JE choisi la PREMIERE
-		// JE JOUE LA PREMIERE
-		// QUAND LA PREMIERE EST FINI JE RELANCE LA FONCTION
-		// QUELLE FONCTION ?
-
-}
-
-
-function playRandomSounds() {
-	var random = Math.floor(Math.random()*4); //0,1,2,3
-
-	switch (random) {
-		case 0:
-			touch = square;
-			break;
-		case 1:
-			touch = triangle;
-			break;
-		case 2:
-			touch = circle;
-			break;
-		case 3:
-			touch = cross;
-			break;
+	function start(){
+	    $('#start').click(function () {
+	        var val = $('#pseudo').val();
+	        if (val != '') {
+	            $('#game-container').attr('style', 'visibility:visible');
+	            $('#game-img').attr('style', 'visibility:visible');
+	            $('.start-container, .bg-img').hide();    
+	        }else{
+	            $('#error').attr('style', 'visibility:visible');
+	        }
+	        console.log(val);
+	    });
 	}
-	touch.play().then(function(data){
-		console.log('playdone');
-		playRandomSounds();
+
+	start();
+
+
+	window.customElements.define('div-touch', Touch);
+	var main = document.getElementById('game-img');
+
+
+	var square = new Touch('square','purple');
+	var triangle = new Touch('triangle','green');
+	var circle = new Touch('circle','orange');
+	var cross = new Touch('cross','blue');
+
+	main.appendChild(square);
+	main.appendChild(triangle);
+	main.appendChild(circle);
+	main.appendChild(cross);
+
+
+	square.addEventListener('touchClicked', function (ev) {
+	    //console.log(ev.detail);
 	});
-}
+	triangle.addEventListener('touchClicked', function (ev) {
+	    //console.log(ev.detail);
+	});
+	circle.addEventListener('touchClicked', function (ev) {
+	    //console.log(ev.detail);
+	});
+	cross.addEventListener('touchClicked', function (ev) {
+	    //console.log(ev.detail);
+	});
+
+	var melodyComputer = [square, circle, circle, triangle, cross, cross, square];
+
+	document.onclick = function(){
+		playMelody(melodyComputer).then(function(d){
+			console.log(d);
+		});
+	};
+
+	function playNote(note) {
+		return note.play();
+	}
+
+
+
+	function playMelody(melody){
+		return new Promise(function(resolve, reject){
+			
+			var i = 0;
+			
+			function play(i) {
+				var note = melody[i];
+				if (!note) {
+					return resolve('Melody ended');
+				}
+				return playNote(note).then(() => {
+					i++;
+					return play(i);
+				});
+			}
+			
+			play(i);
+    	});
+		
+	}
+
+});
